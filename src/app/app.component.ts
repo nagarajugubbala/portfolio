88069947653bd15835f693aec18bdc000eb21e99
nagarajugubbala <nraj888@gmail.com>
2025-02-06 11:50:07 +0530

    Updated portfolio design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  skills = [
+    { name: 'Java', value: 90 },
+    { name: 'Spring Boot', value: 85 },
+    { name: 'Angular', value: 95 },
+    { name: 'React', value: 80 },
+  ];
+
+  projects = [
+    {
+      name: 'Gummadibuilt',
+      description: 'An e-procurement platform for vendors and clients.',
+      image: 'assets/gummadibuilt.png',
+      link: 'https://gummadibuilt.com',
+    },
+    {
+      name: 'Learning Management System',
+      description: 'A platform for managing courses and exams.',
+      image: 'assets/lms.png',
+      link: 'https://example.com/lms',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -10,9 +18,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
